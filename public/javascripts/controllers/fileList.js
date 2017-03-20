1c34999a8dce0c7611ff83aa6cc1998c8e996795
Marcin Matyska <mmatyska@n-educatio.pl>
2017-03-20 11:40:11 +0100

    added  service reading data from remote

--- public/javascripts/controllers/fileList.js
+++ public/javascripts/controllers/fileList.js
@@ -1,18 +1,7 @@
-mainModule.controller('fileList', function($scope){
-    $scope.fileList = 
-        [
-            { id : 1, name : 'file1.jpg' },
-            { id : 2, name : 'file2.jpg' },
-            { id : 3, name : 'file3.jpg' },
-            { id : 4, name : 'file4.jpg' },
-            { id : 5, name : 'file5.jpg' },
-            { id : 6, name : 'file6.jpg' },
-            { id : 7, name : 'file7.jpg' },
-            { id : 8, name : 'file8.jpg' },
-            { id : 9, name : 'file9.jpg' },
-            { id : 10, name : 'file10.jpg' }            
-        ];
+mainModule.controller('fileList', function($scope, $http){
+    $http.get('http://localhost:3000/api').then(function (response) {
+        $scope.fileList = response.data;
+    });
 
-       $scope.isShowableTag = true;
-
+    $scope.isShowableTag = true;
 });
